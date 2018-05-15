c1e3290fd986300827959472435fbf3973cc9e34
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-15 09:19:18 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,29 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
 
+    post '/team' do
+        params.each do |k, v|
+            if k == "name"
+                @team = v
+            elsif k == "coach"
+                @coach = v
+            elsif k == "pg"
+                @pg = v
+            elsif k == "sg"
+                @sg = v
+            elsif k == "pf"
+                @pf = v
+            elsif k == "sf"
+                @sf = v
+            elsif k == "c"
+                @c = v 
+            end
+        end
+
+        erb :team 
+    end
 end
